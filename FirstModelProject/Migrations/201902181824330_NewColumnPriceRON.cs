e1f87a09873c0f535eb02267984a35512a4b9471
Student <student@stepit.loc>
2019-02-18 21:27:12 +0200

    files commit

---
+++ FirstModelProject/Migrations/201902181824330_NewColumnPriceRON.cs
@@ -0,0 +1,16 @@
+namespace FirstModelProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewColumnPriceRON : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ FirstModelProject/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FirstModelProject.Models
+{
+
+
+	public class Movie
+	{
+		public int ID { get; set; }
+		public string Title { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayName("Date of release")]
+
+		public DateTime ReleaseDate { get; set; }
+		public string Genre { get; set; }
+		public decimal Price { get; set; }
+		public decimal PriceRon { get; set; }
+		
+
+	}
+}
